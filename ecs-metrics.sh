279a52671ba254c7e99ff15b5f91a581a18e3de5
Dale Phurrough <dale@hidale.com>
2019-09-26 18:15:03 +0200

    change working dir; remove /tmp need

--- ecs-metrics.sh
+++ ecs-metrics.sh
@@ -1,20 +1,23 @@
 #!/bin/sh -e
 
+WORKDIR="$(mktemp -d -p /mnt/ecs-metrics)"
+cd "$WORKDIR"
+
 # curl --unix-socket /var/run/docker.sock http://dummyhost/v1.24/containers/samba/stats?stream=false
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v3.html
-curl -s ${ECS_CONTAINER_METADATA_URI}/task > /tmp/es_task.json
-curl -s ${ECS_CONTAINER_METADATA_URI}/task/stats > /tmp/es_stats.json
+curl -s ${ECS_CONTAINER_METADATA_URI}/task > es_task.json
+curl -s ${ECS_CONTAINER_METADATA_URI}/task/stats > es_stats.json
 
-ECS_CLUSTER="$(jq -r '.Cluster' /tmp/es_task.json)"
-ECS_SERVICE="$(jq -r '.Family' /tmp/es_task.json)"
-ECS_TASK_ID="$(jq -r '.TaskARN' /tmp/es_task.json | grep -o -E '[^/]+$')"
-ECS_AVAILZONE="$(jq -r '.AvailabilityZone' /tmp/es_task.json)"
+ECS_CLUSTER="$(jq -r '.Cluster' es_task.json)"
+ECS_SERVICE="$(jq -r '.Family' es_task.json)"
+ECS_TASK_ID="$(jq -r '.TaskARN' es_task.json | grep -o -E '[^/]+$')"
+ECS_AVAILZONE="$(jq -r '.AvailabilityZone' es_task.json)"
 
-jq '[.Containers[] | {id:.DockerId,dcName:.Name}]' /tmp/es_task.json > /tmp/es_containers.json
-jq '[.[]]' /tmp/es_stats.json > /tmp/es_stats_array.json
+jq '[.Containers[] | {id:.DockerId,dcName:.Name}]' es_task.json > es_containers.json
+jq '[.[]]' es_stats.json > es_stats_array.json
 
 jq -s '[ .[0] + .[1] | group_by(.id)[] | select(length > 1) | add ]' \
-    /tmp/es_containers.json /tmp/es_stats_array.json > /tmp/es_combo_stats.json
+    es_containers.json es_stats_array.json > es_combo_stats.json
 
 MEM_TOTAL=$(free -b | grep Mem | awk '{print $2}')
 
@@ -38,10 +41,11 @@ jq "[.[] | {time: .read, \
             percentThrottle: (if (.cpu_stats.throttling_data.periods - .precpu_stats.throttling_data.periods) == 0 then 0 else 100 * (.cpu_stats.throttling_data.throttled_periods - .precpu_stats.throttling_data.throttled_periods) / (.cpu_stats.throttling_data.periods - .precpu_stats.throttling_data.periods) end), \
             percentThrottle2: (100 * (.cpu_stats.throttling_data.throttled_time - .precpu_stats.throttling_data.throttled_time) / (.cpu_stats.system_cpu_usage - .precpu_stats.system_cpu_usage)) \
     }]" \
-    /tmp/es_combo_stats.json > /tmp/es_ready_stats.json
+    es_combo_stats.json > es_ready_stats.json
 
 # move to host instance volume, where agent on host will forward to cloudwatch
 EPOCH=$(date --utc +%s)
-mv /tmp/es_ready_stats.json "/mnt/ecs-metrics/${ECS_TASK_ID}-${EPOCH}.json"
+mv es_ready_stats.json "/mnt/ecs-metrics/${ECS_TASK_ID}-${EPOCH}.json"
 
-rm /tmp/es_*.json
+cd /
+rm -rf "$WORKDIR"
