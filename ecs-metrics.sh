dccf3250c6cc9bffde3ae53e64f1a03f10809d3e
Dale Phurrough <dale@hidale.com>
2019-09-27 21:45:11 +0200

    calculate CPU util of task

--- ecs-metrics.sh
+++ ecs-metrics.sh
@@ -13,13 +13,11 @@ ECS_SERVICE="$(jq -r '.Family' es_task.json)"
 ECS_TASK_ID="$(jq -r '.TaskARN' es_task.json | grep -o -E '[^/]+$')"
 ECS_AVAILZONE="$(jq -r '.AvailabilityZone' es_task.json)"
 
-jq '[.Containers[] | {id:.DockerId,dcName:.Name}]' es_task.json > es_containers.json
+jq '.Limits as $tlimits | [.Containers[] | {id:.DockerId, dcName:.Name, taskLimits:$tlimits}]' es_task.json > es_containers.json
 jq '[.[]]' es_stats.json > es_stats_array.json
 
 jq -s '[ .[0] + .[1] | group_by(.id)[] | select(length > 1) | add ]' \
     es_containers.json es_stats_array.json > es_combo_stats.json
-
-MEM_TOTAL=$(free -b | grep Mem | awk '{print $2}')
 
 # https://www.datadoghq.com/blog/how-to-collect-docker-metrics/
 # https://docs.docker.com/config/containers/runmetrics/
@@ -34,9 +32,11 @@ jq "[.[] | {time: .read, \
             containerName: .dcName, \
             currentMem: .memory_stats.usage, \
             maxMem: .memory_stats.max_usage, \
-            percentMem: (100 * .memory_stats.usage / (if (.memory_stats.stats.hierarchical_memory_limit == 9223372036854771712) then $MEM_TOTAL else .memory_stats.stats.hierarchical_memory_limit end)), \
+            percentMemOfTask: (100 * .memory_stats.usage / (if (.memory_stats.stats.hierarchical_memory_limit == 9223372036854771712) then .memory_stats.limit else .memory_stats.stats.hierarchical_memory_limit end)), \
+            percentMemOfHost: (100 * .memory_stats.usage / .memory_stats.limit), \
             tmpfsMem: (.memory_stats.stats.cache - .memory_stats.stats.active_file - .memory_stats.stats.inactive_file), \
             tmpfsMem2: (.memory_stats.stats.active_anon + .memory_stats.stats.inactive_anon - .memory_stats.stats.rss), \
+            percentCpuOfTask: (100 * (.cpu_stats.cpu_usage.total_usage - .precpu_stats.cpu_usage.total_usage) / (.cpu_stats.system_cpu_usage - .precpu_stats.system_cpu_usage) * .cpu_stats.online_cpus / (if (.taskLimits.CPU > 0) then .taskLimits.CPU else .cpu_stats.online_cpus end)), \
             percentCpuOfHost: (100 * (.cpu_stats.cpu_usage.total_usage - .precpu_stats.cpu_usage.total_usage) / (.cpu_stats.system_cpu_usage - .precpu_stats.system_cpu_usage)), \
             percentThrottle: (if (.cpu_stats.throttling_data.periods - .precpu_stats.throttling_data.periods) == 0 then 0 else 100 * (.cpu_stats.throttling_data.throttled_periods - .precpu_stats.throttling_data.throttled_periods) / (.cpu_stats.throttling_data.periods - .precpu_stats.throttling_data.periods) end), \
             percentThrottle2: (100 * (.cpu_stats.throttling_data.throttled_time - .precpu_stats.throttling_data.throttled_time) / (.cpu_stats.system_cpu_usage - .precpu_stats.system_cpu_usage)) \
